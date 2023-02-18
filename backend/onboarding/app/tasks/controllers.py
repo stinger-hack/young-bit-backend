01c3853540d89f09dbfc2f6939326bad965cd7b5
pavivin <pavivin@yandex.ru>
2023-02-18 05:32:23 +0400

    tasks count

--- backend/onboarding/app/tasks/controllers.py
+++ backend/onboarding/app/tasks/controllers.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from onboarding.app.auth.models import Users
-from .views import TaskView
+from .views import TaskView, TaskPayload
 from .models import DepartmentTask, IndividualTask
 from onboarding.auth.oauth2 import get_current_user
 from onboarding.db import get_session
@@ -12,7 +12,7 @@ from onboarding.enums import TaskTypeEnum
 router = APIRouter(dependencies=[Depends(get_current_user)])
 
 
-@router.get("/tasks", response_model=Response[list[TaskView]])
+@router.get("/tasks", response_model=Response[TaskView])
 async def get_news(
     task_type: TaskTypeEnum, user: Users = Depends(get_current_user), session: AsyncSession = Depends(get_session)
 ):
@@ -22,4 +22,7 @@ async def get_news(
         user = await Users.get_by_id(user_id=user.id, session=session)
         result = await DepartmentTask.get_by_department_id(department_id=user.department_id, session=session)
 
-    return Response(body=[TaskView.from_orm(item) for item in result])
+    # TODO: to real data
+    return Response(
+        body=TaskView(tasks_count=18, tasks_finished=12, tasks=[TaskPayload.from_orm(item) for item in result])
+    )
--- backend/onboarding/app/tasks/views.py
+++ backend/onboarding/app/tasks/views.py
@@ -2,10 +2,16 @@ from datetime import datetime
 from onboarding.protocol import BaseModel
 
 
-class TaskView(BaseModel):
+class TaskPayload(BaseModel):
     title: str
     description: str
     spent_time: int | None
     cost: int | None
     created_at: datetime
     updated_at: datetime | None
+
+
+class TaskView(BaseModel):
+    tasks_count: int
+    tasks_finished: int
+    tasks: list[TaskPayload]
