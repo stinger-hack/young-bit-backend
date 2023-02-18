54c777899b3c78102f985d54af6c60b63b8f8ed7
pavivin <pavivin@yandex.ru>
2023-02-19 03:18:23 +0400

    autocommit

--- backend/onboarding/db/base.py
+++ backend/onboarding/db/base.py
@@ -3,6 +3,6 @@ from sqlalchemy.orm import declarative_base
 
 from onboarding.config import settings
 
-engine = create_async_engine(settings.DATABASE_URL, echo=True, future=True, pool_size=100, max_overflow=0)
+engine = create_async_engine(settings.DATABASE_URL, echo=True, future=True, isolation_level="AUTOCOMMIT")
 
 Base = declarative_base()
