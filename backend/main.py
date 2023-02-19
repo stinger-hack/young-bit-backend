d05cf81433858b2b9b2871a96831248c4841cf96
pavivin <pavivin@yandex.ru>
2023-02-19 05:15:54 +0400

    cors

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,6 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["GET", "POST"],
     allow_headers=["*"],
 )
 app.include_router(healthcheck.router, tags=["healthcheck"])
